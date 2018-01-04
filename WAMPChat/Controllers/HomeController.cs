ff6a1e7858aba0934a56b6324206a0f5d5f8d092
aenen <yaroslav.golyuk@yandex.ua>
2018-01-04 13:11:03 +0200

    [✓] 8. Свойства пользователей в ASP.NET Identity (https://professorweb.ru/my/ASP_NET/identity/level1/1_8.php)

--- WAMPChat/Controllers/HomeController.cs
+++ WAMPChat/Controllers/HomeController.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WAMPChat.Infrastructure;
+using WAMPChat.Models;
 
 namespace WAMPChat.Controllers
 {
@@ -18,6 +25,59 @@ namespace WAMPChat.Controllers
         public ActionResult OtherAction()
         {
             return View("Index", GetData("OtherAction"));
+        }
+
+        [Authorize]
+        public ActionResult UserProps()
+        {
+            return View(CurrentUser);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult> UserProps(Cities city)
+        {
+            AppUser user = CurrentUser;
+            user.City = city;
+
+            user.SetCountryFromCity(city);
+
+            await UserManager.UpdateAsync(user);
+            return View(user);
+        }
+
+        private AppUser CurrentUser
+        {
+            get
+            {
+                return UserManager.FindByName(HttpContext.User.Identity.Name);
+            }
+        }
+
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+
+        // Вспомогательный метод, загружающий название элемента перечисления
+        // из атрибута Display
+        [NonAction]
+        public static string GetCityName<TEnum>(TEnum item)
+            where TEnum : struct, IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("Тип TEnum должен быть перечислением");
+            }
+            else
+                return item.GetType()
+                    .GetMember(item.ToString())
+                    .First()
+                    .GetCustomAttribute<DisplayAttribute>()
+                    .Name;
         }
 
         private Dictionary<string, object> GetData(string actionName)
--- WAMPChat/Models/AppUserModels.cs
+++ WAMPChat/Models/AppUserModels.cs
@@ -1,13 +1,61 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace WAMPChat.Models
 {
+    public enum Cities
+    {
+        [Display(Name = "Лондон")]
+        LONDON,
+
+        [Display(Name = "Париж")]
+        PARIS,
+
+        [Display(Name = "Москва")]
+        MOSCOW
+    }
+
+    public enum Countries
+    {
+        [Display(Name = "Не указано")]
+        NONE,
+
+        [Display(Name = "Англия")]
+        ENG,
+
+        [Display(Name = "Франция")]
+        FRA,
+
+        [Display(Name = "Россия")]
+        RUS
+    }
+
     public class AppUser : IdentityUser
     {
-        // Здесь будут указываться дополнительные свойства
+        public Cities City { get; set; }
+        public Countries Country { get; set; }
+
+        public void SetCountryFromCity(Cities city)
+        {
+            switch (city)
+            {
+                case Cities.LONDON:
+                    Country = Countries.ENG;
+                    break;
+                case Cities.PARIS:
+                    Country = Countries.FRA;
+                    break;
+                case Cities.MOSCOW:
+                    Country = Countries.RUS;
+                    break;
+                default:
+                    Country = Countries.NONE;
+                    break;
+            }
+        }
     }
 }
