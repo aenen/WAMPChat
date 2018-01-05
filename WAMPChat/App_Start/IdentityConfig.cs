659c6bbf40016e81731962e672929172d18edcbd
aenen <yaroslav.golyuk@yandex.ua>
2018-01-05 14:42:55 +0200

    [✓] 10. Использование OpenID и OAuth в ASP.NET Identity (https://professorweb.ru/my/ASP_NET/identity/level1/1_10.php)

--- WAMPChat/App_Start/IdentityConfig.cs
+++ WAMPChat/App_Start/IdentityConfig.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
 using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using WAMPChat.Infrastructure;
 
 namespace WAMPChat.App_Start
@@ -16,6 +19,7 @@ namespace WAMPChat.App_Start
         {
             app.CreatePerOwinContext<AppIdentityDbContext>(AppIdentityDbContext.Create);
             app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+
             app.CreatePerOwinContext<AppRoleManager>(AppRoleManager.Create);
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions
@@ -23,6 +27,23 @@ namespace WAMPChat.App_Start
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
             });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
+            {
+                AuthenticationType = "Google",
+                ClientId = "233366183690-ondq9r7h3i7thfh1ppemviunlomprg0o.apps.googleusercontent.com",
+                ClientSecret = "92TaDBnwC67OJcM_kXUBHshh",
+                Caption = "Авторизация через Google+",
+                CallbackPath = new PathString("/GoogleLoginCallback"),
+                AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Passive,
+                SignInAsAuthenticationType = app.GetDefaultSignInAsAuthenticationType(),
+                BackchannelTimeout = TimeSpan.FromSeconds(60),
+                BackchannelHttpHandler = new System.Net.Http.WebRequestHandler(),
+                BackchannelCertificateValidator = null,
+                Provider = new GoogleOAuth2AuthenticationProvider()
+            }
+            );
         }
     }
 }
